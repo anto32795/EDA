9f937b54e73ecda1206c8effbf15dd1ef872f715
anto32795 <anto32795@gmail.com>
2020-11-02 16:45:07 +0100

    added some got

--- src/usecase/practica2/GameOfThrones.java
+++ src/usecase/practica2/GameOfThrones.java
@@ -1,11 +1,70 @@
 package usecase.practica2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import material.Position;
+
+
+import material.Position;
 import material.tree.narytree.LinkedTree;
 
 public class GameOfThrones {
+    List<FamilyMember> listaFamiliares;
+
+    public GameOfThrones() {
+        this.listaFamiliares = new LinkedList<>();
+    }
 
     public void loadFile(String pathToFile){
-        throw new RuntimeException("Not yet implemented");
+        String id, name, surname, genre;
+        Integer age;
+        String linea = "";
+        String limit = "---------------------------------------------------------------------------------------";
+        String[] linea_separada;
+        FamilyMember familiar;
+
+        try{
+            File fichero = new File(pathToFile);
+            Scanner sc = new Scanner(fichero);
+
+            while(sc.hasNextLine()){
+                linea = sc.nextLine();
+                if(linea.contains("--")){
+                    break;
+                }
+                //System.out.println("linea leida: "+linea);
+                linea_separada = linea.split(" ");
+                id = linea_separada[0];
+                name = linea_separada[2];
+                surname = linea_separada[3];
+                genre = linea_separada[4];
+                age = Integer.parseInt(linea_separada[5]);
+
+                //System.out.println("Familiar recibido: "+id+name+surname+genre+linea_separada[5]);
+
+                familiar = new FamilyMember(id, age, name,surname,genre);
+                listaFamiliares.add(familiar );
+
+
+                // Caputurados todos los nodos
+
+            }
+            int num_familias = Integer.parseInt(sc.nextLine());
+            System.out.printf("Num fam : "+num_familias);
+            FamilyMember[] roots = new FamilyMember[num_familias];
+
+            /*for(int i=0; i<num_familias;i++){
+                roots[i] =
+            }*/
+
+            sc.close();
+        }catch (FileNotFoundException e){
+            System.err.println("File not found..");
+            e.getMessage();
+        }
     }
 
     public LinkedTree<FamilyMember> getFamily(String surname){
